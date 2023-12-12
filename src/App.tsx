3bd8788290d19497aea630000ad7bbca908d1c24
Cristian Perafan <1110282139@u.icesi.edu.co>
2023-12-11 22:03:11 -0500

    Feat:Add file and folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import './globals.css'
+import { Routes, Route } from 'react-router-dom'
+
+import './style/globals.css'
+
+import SigninForm from './auth/forms/SigninForm'
+import SignupForm from './auth/forms/SignupForm'
+import AuthContainer from './auth/AuthContainer'
+
+import RootContainer from './root/RootContainer'
+import { Home } from './root/pages'
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <main className='flex h-screen'>
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthContainer/>}>
+          <Route path='/sign-in' element={<SigninForm/>} />
+          <Route path='/sign-up' element={<SignupForm/>} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootContainer/>}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   )
 }
 
---
+++ src/root/RootContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RootContainer() {
+  return (
+    <div>RootContainer</div>
+  )
+}
+
+export default RootContainer
